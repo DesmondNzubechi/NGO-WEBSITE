27cd598ffda54914ff07eca6c77ffd0b5851177e
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-03-24 11:05:33 +0100

    new change

--- Admin/mycode.php
+++ Admin/mycode.php
@@ -1,3 +1,23 @@
 
 
 <?php
+require_once 'database.php';
+
+if (isset($_GET['id'])) {
+    # code...
+$getId = $_GET['id'];
+
+if ($conn) {
+    # code...
+$sql = "DELETE FROM post WHERE postid='$getId'";
+$check = mysqli_query($conn, $sql);
+
+if ($check) {
+    # code...
+    header('Location:  /ngo/admin/post.php');
+}
+
+}
+
+
+}
