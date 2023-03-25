11c1b50bb4784f864f639abdd376a4d1c28510ba
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-03-26 00:44:37 +0100

    landing page

---
+++ index.php
@@ -0,0 +1,83 @@
+<html lang="en">
+<head>
+    <script src="https://cdn.tailwindcss.com"></script>
+    
+	 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/styles.css">
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body class='bg-slate-200 overflow-x-hidden'>
+ <header class='bg-slate-900 flex flex-row items-center justify-around px-[20px] py-[30px]'>
+ <!--<div><h1 class='text-[25px] text-[white] font-bold'>NGO WEBSITE</h1></div>
+
+        <button><i class="fa-solid fa-user bg-gray-500 p-[6px] rounded-full text-[35px] "></i></button>
+  
+-->
+<div class='relative z-[1] '>
+<h1 class='text-[30px] ml-[-20px] text-white font-[700]'>NGO WEBSITE</h1>
+</div>
+
+<ul class='md:relative fixed left-0 top-0 bottom-0 w-[50%] md:pt-[0] pt-[200px]  text-center items-start px-[30px] bg-slate-900 flex-col gap-[10px] flex md:flex-row gap-[20px]'>
+<li class=' bg-slate-900'>
+        <a href="http://" class='text-white text-[25px] uppercase text-[20px] font-[500] hover:underline'>The Problem
+        <i class="fa-solid fa-angle-down"></i>
+             <i class="fa-solid hidden  fa-angle-up"></i>
+        </a>
+        <ul class='absolute bg-white z-[1] flex flex-col w-fit items-start rounded shadow-2xl p-[20px] text-start gap-[20px]'>
+            <li><a href="" class='text-[20px] font-[400]'>Know the Sign</a></li>
+         
+            <li><a href="" class='text-[20px] font-[400]'>Story from Survivors</a></li>
+           
+            <li><a href="" class='text-[20px] font-[400]'>Key Facts, Statics and Resources</a></li>
+        </ul>
+    </li>
+    <li class='relative bg-slate-900'>
+        <a href="http://" class='text-white  text-[20px] font-[500] hover:underline text-[25px] uppercase '>Join fight
+        <i class="fa-solid  fa-angle-down"></i>
+             <i class="fa-solid hidden  fa-angle-up"></i>
+        </a>
+        <ul class=' top-0 hidden'>
+            <li><a href="" class=''>Know the Sign</a></li>
+            <li><a href="" class=''>Story from Survivors</a></li>
+            <li><a href="" class=''>Key Facts, Statics and Resources</a></li>
+        </ul>
+    </li>
+    <li class='relative bg-slate-900'>
+        <a href="http://" class='text-white text-[25px] uppercase  text-[20px] font-[500] hover:underline'>Community
+        <i class="fa-solid  fa-angle-down"></i>
+             <i class="fa-solid hidden  fa-angle-up"></i></a>
+        <ul class=' top-0 hidden'>
+            <li><a href="" class=''>Know the Sign</a></li>
+            <li><a href="" class=''>Story from Survivors</a></li>
+            <li><a href="" class=''>Key Facts, Statics and Resources</a></li>
+        </ul>
+    </li>
+</ul>
+
+
+<div class='flex gap-[20px] flex-row items-center'>
+
+    <a href="" class='bg-white px-[20px] shadow-2xl rounded py-[10px] text-[22px] font-[600] '>Donate</a>
+   
+<div class=' flex flex-row  md:hidden'>
+
+<i class="fa-solid fa-bars text-white  text-[40px]"></i>
+<i class="fa-solid fa-xmark text-white hidden text-[40px]"></i>
+</div>
+</div>
+ </header>
+
+
+
+
+
+
+
+
+
+ </body>
+</html>
