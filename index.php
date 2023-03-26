efeeb635bbd737927984bb497a1608111205d659
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-03-27 00:30:39 +0100

    new change

--- index.php
+++ index.php
@@ -10,8 +10,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body class='bg-slate-200 overflow-x-hidden'>
- <header class='bg-slate-900 flex flex-row items-center justify-around px-[20px] py-[30px]'>
+<body class='bg-slate-50 overflow-x-hidden'>
+ <header class='bg-slate-900 z-[10] fixed left-0 right-0 top-0  flex flex-row items-center justify-around px-[20px] py-[30px]'>
  <!--<div><h1 class='text-[25px] text-[white] font-bold'>NGO WEBSITE</h1></div>
 
         <button><i class="fa-solid fa-user bg-gray-500 p-[6px] rounded-full text-[35px] "></i></button>
@@ -21,13 +21,13 @@
 <h1 class='text-[30px] ml-[-20px] text-white font-[700]'>NGO WEBSITE</h1>
 </div>
 
-<ul class='md:relative fixed left-0 top-0 bottom-0 w-[50%] md:pt-[0] pt-[200px]  text-center items-start px-[30px] bg-slate-900 flex-col gap-[10px] flex md:flex-row gap-[20px]'>
-<li class=' bg-slate-900'>
+<ul class=' md:static fixed sideNav md:left-0 left-[-1000px] ease-out duration-500 top-0 bottom-0 w-[50%] md:pt-[0] pt-[200px]  text-center items-start px-[30px] bg-slate-900 flex-col gap-[10px] flex md:flex-row gap-[20px]'>
+<li class=' showDropDown text-start bg-slate-900'>
         <a href="http://" class='text-white text-[25px] uppercase text-[20px] font-[500] hover:underline'>The Problem
         <i class="fa-solid fa-angle-down"></i>
              <i class="fa-solid hidden  fa-angle-up"></i>
         </a>
-        <ul class='absolute bg-white z-[1] flex flex-col w-fit items-start rounded shadow-2xl p-[20px] text-start gap-[20px]'>
+        <ul class=' md:absolute hidden ease-out duration-500 dropDown bg-white z-[1] flex flex-col w-fit items-start rounded shadow-2xl p-[20px] text-start gap-[20px]'>
             <li><a href="" class='text-[20px] font-[400]'>Know the Sign</a></li>
          
             <li><a href="" class='text-[20px] font-[400]'>Story from Survivors</a></li>
@@ -35,49 +35,110 @@
             <li><a href="" class='text-[20px] font-[400]'>Key Facts, Statics and Resources</a></li>
         </ul>
     </li>
-    <li class='relative bg-slate-900'>
-        <a href="http://" class='text-white  text-[20px] font-[500] hover:underline text-[25px] uppercase '>Join fight
-        <i class="fa-solid  fa-angle-down"></i>
+    <li class=' showDropDown text-start bg-slate-900'>
+        <a href="http://" class='text-white text-[25px] uppercase text-[20px] font-[500] hover:underline'>The Problem
+        <i class="fa-solid fa-angle-down"></i>
              <i class="fa-solid hidden  fa-angle-up"></i>
         </a>
-        <ul class=' top-0 hidden'>
-            <li><a href="" class=''>Know the Sign</a></li>
-            <li><a href="" class=''>Story from Survivors</a></li>
-            <li><a href="" class=''>Key Facts, Statics and Resources</a></li>
+        <ul class=' md:absolute hidden ease-out duration-500 dropDown bg-white z-[1] flex flex-col w-fit items-start rounded shadow-2xl p-[20px] text-start gap-[20px]'>
+            <li><a href="" class='text-[20px] font-[400]'>Know the Sign</a></li>
+         
+            <li><a href="" class='text-[20px] font-[400]'>Story from Survivors</a></li>
+           
+            <li><a href="" class='text-[20px] font-[400]'>Key Facts, Statics and Resources</a></li>
         </ul>
     </li>
-    <li class='relative bg-slate-900'>
-        <a href="http://" class='text-white text-[25px] uppercase  text-[20px] font-[500] hover:underline'>Community
-        <i class="fa-solid  fa-angle-down"></i>
-             <i class="fa-solid hidden  fa-angle-up"></i></a>
-        <ul class=' top-0 hidden'>
-            <li><a href="" class=''>Know the Sign</a></li>
-            <li><a href="" class=''>Story from Survivors</a></li>
-            <li><a href="" class=''>Key Facts, Statics and Resources</a></li>
+    <li class=' showDropDown text-start bg-slate-900'>
+        <a href="http://" class='text-white text-[25px] uppercase text-[20px] font-[500] hover:underline'>The Problem
+        <i class="fa-solid fa-angle-down"></i>
+             <i class="fa-solid hidden  fa-angle-up"></i>
+        </a>
+             <ul class=' md:absolute hidden ease-out duration-500 dropDown bg-white z-[1] flex flex-col w-fit items-start rounded shadow-2xl p-[20px] text-start gap-[20px]'>
+            <li><a href="" class='text-[20px] font-[400]'>Know the Sign</a></li>
+         
+            <li><a href="" class='text-[20px] font-[400]'>Story from Survivors</a></li>
+           
+            <li><a href="" class='text-[20px] font-[400]'>Key Facts, Statics and Resources</a></li>
         </ul>
     </li>
 </ul>
-
-
 <div class='flex gap-[20px] flex-row items-center'>
 
     <a href="" class='bg-white px-[20px] shadow-2xl rounded py-[10px] text-[22px] font-[600] '>Donate</a>
    
 <div class=' flex flex-row  md:hidden'>
 
-<i class="fa-solid fa-bars text-white  text-[40px]"></i>
-<i class="fa-solid fa-xmark text-white hidden text-[40px]"></i>
+<i class="fa-solid fa-bars text-white open  text-[40px]"></i>
+<i class="fa-solid fa-xmark text-white close hidden text-[40px]"></i>
 </div>
 </div>
  </header>
 
+<main>
+    <div class="pt-[200px] flex justify-center  text-start  items-center px-[20px] bg-cover relative after:absolute   h-[100vh] after:opacity-[70%] after:top-0 after:left-0 after:right-0 after:h-[100%] after:bg-slate-900 bg-[url('./admin/img/ng.jpg')] pb-[100px]">
+        <div class='flex flex-col max-w-[700px] gap-[40px] '>
+        <div class=''>
+        <h1 class='relative text-white text-[30px] md:text-[40px] uppercase font-[700] z-[1]'>Donor-funded tech to take down human traffickers.</h1>
+    </div>
+    <div>
+        <p class='relative text-slate-200 text-[20px] font-[500]  z-[1] '>DeliverFund equips law enforcement with breakthrough technology to fight injustice at scale.</p>
+    </div>
+    <div class='flex flex-row gap-[20px]'>
+        <a href="" class='relative bg-slate-50 px-[10px] py-[5px] rounded text-slate-900 text-[30px] shadow-2xl  z-[1]'>Contact Us</a>
+        <a href="" class='relative bg-slate-900 px-[10px] py-[5px] rounded text-white text-[30px] shadow-2xl z-[1]'>Support Us</a>
+    </div>
+        </div>
+    
+    </div>
+
+<div class='py-[20px] px-[20px] min-h-[100vh] flex flex-col gap-[30px] md:flex-row justify-around items-center '>
+   
+        <img class='rounded' src="./admin/img/ngpics.jpg" alt="" srcset="">
+   
+    <div class='flex flex-col  gap-[30px] max-w-[600px]'>
+        <h1 class='font-[600] leading-[140%] uppercase text-[30px]'>Join our community to protect women and children</h1>
+        <p class='font-[400]   text-[20px]'>Human traffickers hijack human dignity. We fight to protect women and children from predators, but we can’t do it without your support. If you are passionate about supporting real-world solutions to this complex crisis, we’re honored to have you join this fight.</p>
+        <a href="" class='relative bg-slate-900 px-[10px] py-[5px] rounded text-white w-fit text-[30px] shadow-2xl z-[1]'>Join The Fight</a>
+    </div>
+</div>
+
+
+<div class='py-[50px] px-[20px] bg-slate-700 min-h-[100vh] flex flex-col gap-[30px] md:flex-row justify-around items-center '>
+   
+        <img class='rounded' src="./admin/img/ngpics.jpg" alt="" srcset="">
+   
+    <div class='flex flex-col  gap-[30px] max-w-[600px]'>
+        <h1 class='font-[600] text-slate-300 leading-[140%] uppercase text-[30px]'>Our team has special ops chops</h1>
+        <p class='font-[400] text-slate-400  text-[20px]'>We’re a team of special ops veterans, former intelligence professionals, former law enforcement, analysts and engineers.</p>
+        <p class='font-[400]  text-slate-400 text-[20px]'>We’re data-driven and on a mission to end the horrors of human trafficking.</p>
+        <a href="" class='relative bg-slate-900 px-[10px] py-[5px] rounded text-white w-fit text-[30px] shadow-xl z-[1]'>About Us</a>
+    </div>
+</div>
+
+<div class=' px-[20px] py-[100px] gap-[30px] flex flex-col md:flex-row justify-around items-start '>
+    <div class='flex flex-col  gap-[20px] '>
+        <h1 class='text-[30px] font-[600] uppercase'>Receive updates from Us</h1>
+        <p class='text-[25px] font-[400] '>Subscribe to our monthly newsletter.</p>
+       <p class='text-[25px] font-[400]'>We'll help you stay informed on our fight against human trafficking. </p>
+    </div>
+    
+    <form class='flex  flex-col   gap-[10px] items-start' action="" method="post">
+        
+        <label class='text-[30px] font-[300] mb-[-15px] flex flex-row gap-[10px] items-center '  for="email"><i class="fa-solid fa-envelope"></i> <span> Email</span></label>
+        <input class=' py-[10px] w-[400px] md:w-[600px]  px-[20px] outline-0 rounded text-[20px] bg-transparent border shadow-2xl ' type="email" name="email" id="email" placeholder='Input Your Email '>
+   
+    <input class='relative bg-slate-900 px-[10px] py-[5px] rounded text-white w-full  text-[30px] shadow-xl z-[1]' type="button" value="Subscribe">
+</form>
+           
+</div>
+
+</main>
 
 
 
 
 
 
-
-
+<script src="javascript/index.js"></script>
  </body>
 </html>
---
+++ javascript/index.js
@@ -0,0 +1,35 @@
+let open = document.querySelector('.open');
+let close = document.querySelector('.close');
+let sidemenu = document.querySelector('.sideNav');
+let showDrop = document.querySelectorAll('.ShowDropDown');
+
+
+showDrop.forEach(showD => {
+    showD.addEventListener('mouseover', () => {
+     let getList = showD.children[1];
+     getList.style.display = 'flex';
+    });
+});
+
+
+showDrop.forEach(showD => {
+    showD.addEventListener('mouseout', () => {
+     let getList = showD.children[1];
+     getList.style.display = 'none';
+    });
+});
+
+
+
+open.addEventListener('click', () => {
+    close.style.display = 'block';
+    open.style.display = 'none';
+    sidemenu.style.left = '0';
+})
+
+close.addEventListener('click', () => {
+    close.style.display = 'none';
+    open.style.display = 'block';
+    sidemenu.style.left = '-1000px';
+});
+
