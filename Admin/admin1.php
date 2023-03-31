aec17b12ec0a63ecf7aa0ec991a91108fc630646
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-03-31 15:30:25 +0100

    new change

--- Admin/admin1.php
+++ Admin/admin1.php
@@ -1,4 +1,19 @@
+
+
+
 <?php include 'admin.header.php'; ?>
+
+<?php
+
+$checkSub = "SELECT * FROM subscribers";
+$confirmSub = mysqli_query($conn, $checkSub);
+$totalSub = mysqli_num_rows($confirmSub);
+
+
+$checkMsg = "SELECT * FROM messages";
+$confirmMsg = mysqli_query($conn, $checkMsg);
+$totalMsg = mysqli_num_rows($confirmMsg);
+?>
 
  <div class='w-full'>
   <div class='text-center m-[20px]'><h1 class='text-[30px] font-[700] md:ml-[150px] '>Welcome To Admin Dashboard</h1></div>
@@ -6,7 +21,7 @@
     <div class='flex justify-center'>
 <div class='p-[20px] rounded-lg  min-w-[300px]  bg-gradient-to-r from-slate-900 to-sky-900 shadow-2xl  py-[30px]  text-center  h-fit  '>
  
-  <h1 class='text-[30px] font-bold  py-[7px] px-[20px] '>1</h1>
+  <h1 class='text-[30px] font-bold  py-[7px] px-[20px] '><?php echo $totalSub ?></h1>
     <p class='text-[30px]  uppercase my-[20px] font-[700]'>Subscribers</p>
     <!--<a href="" class=" bg-gradient-to-r from-purple-500 to-blue-500 font-[500] rounded px-[20px] py-[5px] text-[30px]">View</a>-->
 </div> 
@@ -23,7 +38,7 @@
 <div class='flex justify-center'>
 <div class='p-[20px] rounded-lg min-w-[300px]   bg-gradient-to-r from-slate-900 to-sky-900 shadow-2xl  py-[50px]  text-center  h-fit  '>
     <div class=''>
-    <h1 class='text-[30px] font-bold  py-[7px] px-[20px]  rounded-full'>5</h1>
+    <h1 class='text-[30px] font-bold  py-[7px] px-[20px]  rounded-full'><?php echo $totalMsg ?></h1>
     
     </div>
     <p class='text-[30px] uppercase font-[700]'>Messages</p>
