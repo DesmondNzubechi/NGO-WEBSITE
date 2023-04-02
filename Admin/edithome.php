f29240174dcc476451b580d11b74828da5a49664
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-04-02 01:31:51 +0100

    new change

--- Admin/edithome.php
+++ Admin/edithome.php
@@ -2,6 +2,34 @@
 
 
 
+<div class='px-[30px]  '>
+<form class='md:ml-[150px] flex flex-col gap-[20px]' action="\index.php" method="post">
+
+<div class='md:ml-[150px]'>
+    <textarea name="" id="" class='h-[200px] font-[700] p-[20px] flex uppercase text-white bg-slate-700 items-center rounded-lg shadow-lg  overflow-x-auto text-[20px] w-[90%]'> DONOR-FUNDED TECH TO TAKE DOWN HUMAN TRAFFICKER.</textarea>
+</div>
+<div class='md:ml-[150px]'>
+    <textarea name="" id="" class='h-[150px] font-[500] p-[20px] flex  text-white bg-slate-700 items-center rounded-lg shadow-lg  overflow-x-auto text-[20px] w-[90%]'> DeliverFund equips law enforcement with breakthrough technology to fight injustice at scale.</textarea>
+</div>
+<div class='flex flex-row text-center md:ml-[150px]' gap-[10px]'>
+    <input type="text" class='text-[20px] w-fit text-center p-[10px] rounded shadow-xl text-slate-900 font-[500] outline-0' name="headerC" value='Contact Us' id="">
+    <input type="text" class='text-[20px] w-fit text-center p-[10px] rounded shadow-xl bg-slate-900 text-slate-50 font-[500] outline-0' name="headerC" value='Support Us' id="">
+</div>
+<div class=' flex flex-col gap-[20px] md:flex-row justify-center'>
+    <div>
+    <input type="file" name="" id="">
+    <img src="" alt="" class='w-[200px] h-[relative]' srcset="./img/ngpics.jpg">
+    </div>
+    <div >
+    <textarea name="" id="" class='h-[100px] font-[700] p-[20px] flex uppercase text-white bg-slate-700 items-center rounded-lg shadow-lg  overflow-x-auto text-[20px] w-[100%]'> DONOR-FUNDED TECH TO TAKE DOWN HUMAN TRAFFICKER.</textarea>
+    <textarea name="" id="" class='h-[200px] font-[700] p-[20px] flex uppercase text-white bg-slate-700 items-center rounded-lg shadow-lg  overflow-x-auto text-[20px] w-[100%]'> DONOR-FUNDED TECH TO TAKE DOWN HUMAN TRAFFICKER.</textarea>
+     <input type="text" class='text-[20px] w-fit text-center p-[10px] rounded shadow-xl bg-slate-900 text-slate-50 font-[500] outline-0' name="headerC" value='Support Us' id="">
+
+</div>
+</div>
+</form>
+</div>
+
 
 
 
