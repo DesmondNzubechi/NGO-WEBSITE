8d0b9452bf57fc0745737489cfc149e0c7167d13
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-04-02 05:39:56 +0100

    new change

--- Admin/edithome.php
+++ Admin/edithome.php
@@ -11,11 +11,11 @@
 <div class='md:ml-[150px]'>
     <textarea name="" id="" class='h-[150px] font-[500] p-[20px] flex  text-white bg-slate-700 items-center rounded-lg shadow-lg  overflow-x-auto text-[20px] w-[90%]'> DeliverFund equips law enforcement with breakthrough technology to fight injustice at scale.</textarea>
 </div>
-<div class='flex flex-row text-center md:ml-[150px]' gap-[10px]'>
+<div class='flex md:flex-row flex-col  gap-5 text-center md:ml-[150px]' gap-[10px]'>
     <input type="text" class='text-[20px] w-fit text-center p-[10px] rounded shadow-xl text-slate-900 font-[500] outline-0' name="headerC" value='Contact Us' id="">
     <input type="text" class='text-[20px] w-fit text-center p-[10px] rounded shadow-xl bg-slate-900 text-slate-50 font-[500] outline-0' name="headerC" value='Support Us' id="">
 </div>
-<div class=' flex flex-col gap-[20px] md:flex-row justify-center'>
+<div class=' flex flex-col md:ml-[80px] gap-[20px] md:flex-row justify-center'>
     <div class='flex flex-col'>
     <input type="file"  value="" class='outline-none p-[10px] bg-white file:bg-white file:border-0 file: rounded text-[20px]' name="img" id=""/> <input type='hidden' name='old' class='text-slate-200' value='<?=$datas['postimg1'];?>' />
     <img src="" alt="" class='' srcset="./img/ngpics.jpg">
@@ -23,7 +23,23 @@
     <div class='flex flex-col gap-5'>
     <textarea name="" id="" class='h-[100px] md:w-[500px] font-[700] p-[20px] flex uppercase text-white bg-slate-700 items-center rounded-lg shadow-lg  overflow-x-auto text-[20px] w-[100%]'> JOIN OUR COMMUNITY TO PROTECT WOMEN AND CHILDREN.</textarea>
     <textarea name="" id="" class='h-[200px]  md:w-[500px] font-[500] p-[20px] flex  text-white bg-slate-700 items-center rounded-lg shadow-lg  overflow-x-auto text-[15px] w-[100%]'>Human traffickers hijack human dignity. We fight to protect women and children from predators, but we can’t do it without your support. If you are passionate about supporting real-world solutions to this complex crisis, we’re honored to have you join this fight.</textarea>
-     <input type="text" class='text-[20px] w-fit text-center p-[10px] rounded shadow-xl bg-slate-900 text-slate-50 font-[500] outline-0' name="headerC" value='Support Us' id="">
+     <input type="text" class='text-[20px] w-fit text-center p-[10px] rounded shadow-xl bg-slate-900 text-slate-50 font-[500] outline-0' name="headerC" value='Join The Fight' id="">
+
+</div>
+</div>
+
+<div class=' flex flex-col md:ml-[80px]  gap-[20px] md:flex-row justify-center'>
+    <div class='flex flex-col'>
+    <input type="file"  value="" class='outline-none p-[10px] bg-white file:bg-white file:border-0 file: rounded text-[20px]' name="img" id=""/> <input type='hidden' name='old' class='text-slate-200' value='<?=$datas['postimg1'];?>' />
+    <img src="" alt="" class='' srcset="./img/ngpics.jpg">
+    </div>
+    <div class='flex flex-col gap-5'>
+    <textarea name="" id="" class='h-[70px] md:w-[500px] font-[700] p-[20px] flex uppercase text-white bg-slate-700 items-center rounded-lg shadow-lg  overflow-x-auto text-[20px] w-[100%]'> OUR TEAM HAS SPECIAL OPS CHOPS.</textarea>
+    <textarea name="" id="" class='h-[100px]  md:w-[500px] font-[500] p-[20px] flex  text-white bg-slate-700 items-center rounded-lg shadow-lg  overflow-x-auto text-[15px] w-[100%]'> We’re a team of special ops veterans, former intelligence professionals, former law enforcement, analysts and engineers..</textarea>
+    <textarea name="" id="" class='h-[150px]  md:w-[500px] font-[500] p-[20px] flex  text-white bg-slate-700 items-center rounded-lg shadow-lg  overflow-x-auto text-[15px] w-[100%]'>We’re data-driven and on a mission to end the horrors of human trafficking.</textarea>
+
+
+     <input type="text" class='text-[20px]  w-fit text-center p-[10px] rounded shadow-xl bg-slate-900 text-slate-50 font-[500] outline-0' name="headerC" value='About Us' id="">
 
 </div>
 </div>
