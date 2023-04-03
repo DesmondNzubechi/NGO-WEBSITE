b12e7929cdaab0632bd868ca953f7220f8f4c3cb
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-04-03 21:17:08 +0100

    new change

--- Admin/edithome.php
+++ Admin/edithome.php
@@ -2,8 +2,29 @@
 
 
 <?php
+if (isset('updatehome')) {
+    # code...
+$hero1 = $_POST['Hero1'];
+$hero2 = $_POST['Hero2'];
+$heroc = $_POST['HeroC'];
+$heros = $_POST['HeroS'];
+$joinfightimg = $_POST['joinfightimg'];
+$joinfightheader = $_POST['joinfightheader'];
+$joinfightdetail = $_POST['joinfightdetail'];
+$joinfightbtn = $_POST['joinfightbtn'];
+$ourteamimg = $_POST['ourteamimg'];
+$ourteamheader = $_POST['ourteamheader'];
+$ourteamdetails = $_POST['ourteamdetails'];
+$ourteambtn = $_POST['ourteambtn'];
+$emailheader = $_POST['emailheader'];
+$emailp1 = $_POST['emailp1'];
+$emailp2 = $_POST['emailp2'];
+$emailbtn = $_POST['emailbtn'];
 
 
+
+
+}
 
 
 $sql = "SELECT * FROM home";
@@ -35,38 +56,41 @@ foreach ($check as $checked) {
     </div>
     <div class='flex flex-col gap-5'>
     <textarea name="joinfightheader" id="" class='h-[100px] md:w-[500px] font-[700] p-[20px] flex uppercase text-white bg-slate-700 items-center rounded-lg shadow-lg  overflow-x-auto text-[20px] w-[100%]'> <?= $checked['joinfightheader'] ?></textarea>
-    <textarea name="" id="" class='h-[200px]  md:w-[500px] font-[500] p-[20px] flex  text-white bg-slate-700 items-center rounded-lg shadow-lg  overflow-x-auto text-[15px] w-[100%]'><?= $checked['joinfightdetail'] ?></textarea>
-     <input type="text" class='text-[20px] w-fit text-center p-[10px] rounded shadow-xl bg-slate-900 text-slate-50 font-[500] outline-0' name="headerC" value='<?= $checked['joinfightbtn'] ?>' id="">
+    <textarea name="joinfightdetail" id="" class='h-[200px]  md:w-[500px] font-[500] p-[20px] flex  text-white bg-slate-700 items-center rounded-lg shadow-lg  overflow-x-auto text-[15px] w-[100%]'><?= $checked['joinfightdetail'] ?></textarea>
+     <input type="text" class='text-[20px] w-fit text-center p-[10px] rounded shadow-xl bg-slate-900 text-slate-50 font-[500] outline-0' name="joinfightbtn" value='<?= $checked['joinfightbtn'] ?>' id="">
 
 </div>
 </div>
 <hr>
 <div class=' flex flex-col md:ml-[80px]  gap-[20px] md:flex-row justify-center'>
     <div class='flex flex-col'>
-    <input type="file"  value="" class='outline-none p-[10px] bg-white file:bg-white file:border-0 file: rounded text-[20px]' name="img" id=""/> <input type='hidden' name='old' class='text-slate-200' value='<?= $checked['ourteamimg'] ?>' />
+    <input type="file"  value="" class='outline-none p-[10px] bg-white file:bg-white file:border-0 file: rounded text-[20px]' name="ourteamimgnew" id=""/> <input type='hidden' name='ourteamimgold' class='text-slate-200' value='<?= $checked['ourteamimg'] ?>' />
     <img src="" alt="" class='' srcset="./img/<?= $checked['ourteamimg'] ?>">
     </div>
     <div class='flex flex-col gap-5'>
-    <textarea name="" id="" class='h-[70px] md:w-[500px] font-[700] p-[20px] flex uppercase text-white bg-slate-700 items-center rounded-lg shadow-lg  overflow-x-auto text-[20px] w-[100%]'> <?= $checked['ourteamheader'] ?></textarea>
-    <textarea name="" id="" class='h-[100px]  md:w-[500px] font-[500] p-[20px] flex  text-white bg-slate-700 items-center rounded-lg shadow-lg  overflow-x-auto text-[15px] w-[100%]'> <?= $checked['ourteamdetails'] ?> </textarea>
+    <textarea name="ourteamheader" id="" class='h-[70px] md:w-[500px] font-[700] p-[20px] flex uppercase text-white bg-slate-700 items-center rounded-lg shadow-lg  overflow-x-auto text-[20px] w-[100%]'> <?= $checked['ourteamheader'] ?></textarea>
+    <textarea name="ourteamdetails" id="" class='h-[100px]  md:w-[500px] font-[500] p-[20px] flex  text-white bg-slate-700 items-center rounded-lg shadow-lg  overflow-x-auto text-[15px] w-[100%]'> <?= $checked['ourteamdetails'] ?> </textarea>
    <!-- <textarea name="" id="" class='h-[150px]  md:w-[500px] font-[500] p-[20px] flex  text-white bg-slate-700 items-center rounded-lg shadow-lg  overflow-x-auto text-[15px] w-[100%]'>We’re data-driven and on a mission to end the horrors of human trafficking.</textarea>-->
 
 
-     <input type="text" class='text-[20px]  w-fit text-center p-[10px] rounded shadow-xl bg-slate-900 text-slate-50 font-[500] outline-0' name="headerC" value='<?= $checked['ourteambtn'] ?>' id="">
+     <input type="text" name='ourteambtn' class='text-[20px]  w-fit text-center p-[10px] rounded shadow-xl bg-slate-900 text-slate-50 font-[500] outline-0'  value='<?= $checked['ourteambtn'] ?>' id="">
 
 </div>
 </div>
 <hr>
 <div class='md:ml-[130px] justify-center flex flex-col  gap-5'>
     <div class=' flex flex-col md:flex-row gap-5'>
-        <input type="text"  class='h-[70px] md:w-[500px] font-[700] p-[20px] flex uppercase text-700 items-center rounded-lg shadow-lg  overflow-x-auto text-[20px] w-[100%]' name="" value='<?= $checked['emailheader'] ?>' id="">
-        <input type="text"  class='h-[70px] md:w-[500px] font-[500] p-[20px] flex border text-700 items-center rounded-lg shadow-lg  overflow-x-auto text-[20px] w-[100%]' name="" value='<?= $checked['emailp1'] ?>' id="">
+        <input type="text"  class='h-[70px] md:w-[500px] font-[700] p-[20px] flex uppercase text-700 items-center rounded-lg shadow-lg  overflow-x-auto text-[20px] w-[100%]' name="emailheader" value='<?= $checked['emailheader'] ?>' id="">
+        <input type="text"  class='h-[70px] md:w-[500px] font-[500] p-[20px] flex border text-700 items-center rounded-lg shadow-lg  overflow-x-auto text-[20px] w-[100%]' name="emailp1" value='<?= $checked['emailp1'] ?>' id="">
     </div>
     <div class='flex flex-col md:flex-row gap-5'>
-        <textarea type="text" name=""  class='h-[120px] md:w-[500px] font-[500] p-[20px] flex  text-slate-700 items-center rounded-lg shadow-lg  overflow-x-auto text-[18px] w-[100%]'  id=""><?= $checked['emailp2'] ?></textarea>
-        <input type="text" class='text-[20px]  w-fit text-center p-[10px] rounded shadow-xl bg-slate-900 text-slate-50 font-[500] outline-0' name="headerC" value='<?= $checked['emailbtn'] ?>' id="">
+        <textarea type="text"  class='h-[120px] md:w-[500px] font-[500] p-[20px] flex  text-slate-700 items-center rounded-lg shadow-lg  overflow-x-auto text-[18px] w-[100%]' name="emailp2"  id=""><?= $checked['emailp2'] ?></textarea>
+        <input type="text" class='text-[20px]  w-fit text-center p-[10px] rounded shadow-xl bg-slate-900 text-slate-50 font-[500] outline-0' name="emailbtn" value='<?= $checked['emailbtn'] ?>' id="">
 
     </div>
+</div>
+<div class='md:ml-[130px] justify-center flex'>
+<input type="text" class='text-[20px]  w-fit text-center p-[10px] rounded shadow-xl bg-green-500 text-slate-50 font-[500] outline-0' name="updatehome" value='Update Change' id="">
 </div>
 </form>
 </div>
