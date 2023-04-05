abc418a7f10ae9695a66d47beecffb3fd919b3fd
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-04-05 23:16:42 +0100

    new change

--- Admin/editadminprofile.php
+++ Admin/editadminprofile.php
@@ -19,7 +19,7 @@ if ($sqlcheck) {
         }
     }
     
-$oldpwdErr = $confirmpwdErr = $emptynewpwd = $emptyoldpwd = $emptyconfirmpwd = '';
+$totalErr = $confirmpwdErr = $oldpwd = $newpwd = $confirmnewpwd =  $oldpwdErr = $emptynewpwd = $emptyoldpwd = $emptyconfirmpwd = '';
 
     if (isset($_POST['update'])) {
         # code...
@@ -28,6 +28,38 @@ $newpwd = $_POST['newpwd'];
 $confirmnewpwd = $_POST['confirmnewpwd'];
 
 
+if(empty($oldpwd)) {
+    # code...
+    $emptyoldpwd = 'Please input old password';
+} elseif ($oldpwd !== $adminpwd) {
+    # code...
+    $oldpwdErr = 'Password does\'nt match with old one';
+} elseif (empty($newpwd)) {
+    # code...
+    $emptyoldpwd = 'Please input new password';
+} elseif (empty($confirmnewpwd)) {
+    # code...
+    $emptyoldpwd = 'Please confirm new password';
+} elseif ($newpwd !== $confirmnewpwd) {
+    # code...
+    $confirmpwdErr = 'Password does not match';
+
+} else {
+    //$hashedp = password_hash($newpwd, PASSWORD_DEFAULT);
+    $sqlup = "UPDATE addt SET `adpw`='$$newpwd' WHERE `adid`=1";
+    $checkup = mysqli_query($conn, $sqlup);
+
+    if ($checkup) {
+        # code...
+        header('Location: admin/adminprofile.php');
+    } else {
+        # code...
+
+        $totalErr = 'Something went wrong please try again';
+    };
+    
+
+}
 
     }
 
@@ -54,15 +86,21 @@ $confirmnewpwd = $_POST['confirmnewpwd'];
 
 <div class='flex flex-col items-start gap-0'>
     <label class='text-[20px]  text-white font-[400]' for="oldpwd">Old Password</label>
-    <input  placeholder='Input Old Password' class='bg-slate-900 p-2 text-[20px] text-white outline-none rounded border-b w-full' type="text" name="oldpwd" id="">
+    <input  placeholder='Input Old Password' value='<?php echo $oldpwd ?>' class='bg-slate-900 p-2 text-[20px] text-white outline-none rounded border-b w-full' type="password" name="oldpwd" id="">
+    <span class='text-red-500 font-[500] text-[20px]'><?php echo $emptyoldpwd ?></span>
+    <span class='text-red-500 font-[500] text-[20px]'><?php echo $oldpwdErr ?></span>
 </div>
 <div class='flex flex-col items-start gap-0'>
     <label class='text-[20px]  text-white font-[400]' for="newpwd">New Password</label>
-    <input  placeholder='Input New Password' class='bg-slate-900 p-2 text-[15px] text-white outline-none rounded border-b w-full' type="text" name="newpwd" id="">
+    <input value='<?php echo $newpwd ?>'  placeholder='Input New Password' class='bg-slate-900 p-2 text-[15px] text-white outline-none rounded border-b w-full' type="password" name="newpwd" id="">
+    <span class='text-red-500 font-[500] text-[20px]'><?php echo $emptynewpwd ?></span>
+   
 </div>
 <div class='flex flex-col items-start gap-0'>
     <label class='text-[20px]  text-white font-[400]' for="oldpwd">Confirm Password</label>
-    <input  placeholder='Confirm New Email' class='bg-slate-900  p-2 text-[15px] text-white outline-none rounded border-b w-full' type="text" name="confirmoldpwd" id="">
+    <input  value='<?php echo $confirmnewpwd ?>' placeholder='Confirm New Email' class='bg-slate-900  p-2 text-[15px] text-white outline-none rounded border-b w-full' type="password" name="confirmnewpwd" id="">
+    <span class='text-red-500 font-[500] text-[20px]'><?php echo $emptyconfirmpwd ?></span>
+    <span class='text-red-500 font-[500] text-[20px]'><?php echo $confirmpwdErr ?></span>
 </div>
 
 <button type="submit" name='update' class='bg-green-700 text-[20px]  w-full text-white font-[700]  py-[5px] rounded shadow-2xl '>Update Change</button>
